c0d0dcf556087a2c5ad410df222b235071da83c3
Abhinav Telukunta <abhi.telukunta@gmail.com>
2025-03-02 11:22:07 -0500

    changed few fields in subscriptions table

--- Backend/Jobscoop/internal/models/subscription.go
+++ Backend/Jobscoop/internal/models/subscription.go
@@ -8,22 +8,23 @@ import (
 // CreateResetTokensTable creates the reset_tokens table if it does not exist
 func CreateSubscriptionTable() {
 	query := `
-	CREATE TABLE IF NOT EXISTS subscription (
+	CREATE TABLE IF NOT EXISTS subscriptions (
 		id SERIAL PRIMARY KEY,
 		User_Id INT NOT NULL,
 	    Company_Id INT NOT NULL,
-	    Careersite_Id INT[] NOT NULL,
-	    Role_Id INT[] NOT NULL,
+	    Career_site_Ids INT[] NOT NULL,
+	    Role_Ids INT[] NOT NULL,
 	    Disabled BOOLEAN NOT NULL DEFAULT FALSE,
 	    Interest_Time TIMESTAMP,
 
 		CONSTRAINT fk_user FOREIGN KEY (User_Id) REFERENCES Users(Id) ON DELETE CASCADE,
-	    CONSTRAINT fk_company FOREIGN KEY (Company_Id) REFERENCES Company(Company_ID) ON DELETE CASCADE
+	    CONSTRAINT fk_company FOREIGN KEY (Company_Id) REFERENCES Companies(Id) ON DELETE CASCADE,
+		CONSTRAINT unique_user_company UNIQUE (User_Id, Company_Id)
 	);
 	`
 
 	_, err := db.DB.Exec(query)
 	if err != nil {
-		log.Fatalf("Error creating company table: %v", err)
+		log.Fatalf("Error creating subscriptions table: %v", err)
 	}
 }
